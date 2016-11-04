ae8a5208bd616b78e17da825c1f9266831862a6f
Adam Baker <adam.baker97202@gmail.com>
2016-11-04 09:35:05 -0700

    completed the functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@
 //If statements and i++ to increment through the toppings and size
 
 //business logic
+
+function Order (size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Order.prototype.
